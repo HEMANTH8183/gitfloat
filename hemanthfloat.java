75a82764a7494f9f9fc9b6fc752a39e1557c4eb1
HEMANTH8183 <hemanth8183.xworkz@gmail.com>
2021-12-21 07:59:12 +0530

    float file

---
+++ hemanthfloat.java
@@ -0,0 +1,18 @@
+class Hemanth {
+	public static void main (String []args){
+	double []friends = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.10};
+	System.out.println(friends[1]);	
+	System.out.println(friends[0]);
+	System.out.println(friends[2]);
+	System.out.println(friends[3]);
+	System.out.println(friends[4]);
+	System.out.println(friends[5]);
+	System.out.println(friends[6]);
+	System.out.println(friends[7]);
+	System.out.println(friends[8]);
+	System.out.println(friends[9]);
+	System.out.println(friends[10]);
+
+	}
+}
+	
